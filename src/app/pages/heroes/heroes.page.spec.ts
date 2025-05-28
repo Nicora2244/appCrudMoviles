e0e00d7970b2b582feddf979c07441b11d6262ed
Johann2023 <124364320+Johann2023@users.noreply.github.com>
2025-05-28 08:48:54 -0500

    Diasabado

--- src/app/pages/heroes/heroes.page.spec.ts
+++ src/app/pages/heroes/heroes.page.spec.ts
@@ -1,3 +1,4 @@
+/*
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeroesPage } from './heroes.page';
 
@@ -6,6 +7,38 @@ describe('HeroesPage', () => {
   let fixture: ComponentFixture<HeroesPage>;
 
   beforeEach(() => {
+    fixture = TestBed.createComponent(HeroesPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+*/
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeroesPage } from './heroes.page';
+import { provideRouter } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { HeroesBDService } from 'src/app/services/heroes-bd.service';
+
+describe('HeroesPage', () => {
+  let component: HeroesPage;
+  let fixture: ComponentFixture<HeroesPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HeroesPage], // Standalone component
+      providers: [
+        { provide: HeroesBDService, useValue: { getHeroes: () => of([]) } },
+        { provide: ActivatedRoute, useValue: { snapshot: { queryParamMap: { get: () => null } } } },
+        provideRouter([]),
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HeroesPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
